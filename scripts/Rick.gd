b1df33ee9a3b3ea703f18a21cc5215463e7004fc
Çağlar KOPARIR <ckoparir@gmail.com>
2021-03-08 00:56:53 +0300

    jump effect added...

--- scripts/Rick.gd
+++ scripts/Rick.gd
@@ -1,16 +1,22 @@
 extends KinematicBody2D
 
 var velocity = Vector2(0,0)
-export var Speed = 150
+export var Speed = 180
+const GRAVITY = 30
+const JUMP_FORCE = -500
 
 func _physics_process(delta):
 	if Input.is_action_pressed("left"):
 		velocity.x = -Speed
-	if Input.is_action_pressed("right"):	
+	if Input.is_action_pressed("right"):
 		velocity.x = Speed
+		
+	velocity.y = velocity.y + GRAVITY
 	
-	velocity.y = 300
-	move_and_slide(velocity)	
-	velocity.x = lerp(velocity.x,0,0.15)
+	if Input.is_action_pressed("jump"):
+		velocity.y = JUMP_FORCE
+		
+	velocity = move_and_slide(velocity)	
+	velocity.x = lerp(velocity.x,0,0.1)
 
 
